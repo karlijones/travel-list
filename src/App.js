8bfd45452121f0f04e58f7ab5446bda834bf90a4
karlijones <karlijones1984@gmail.com>
2024-10-16 21:03:01 -0500

    adding static components to functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 export default function App() {}
 
 function Logo() {
-  return <h1>🏝️ Far Away< 🧳/h1>;
+  return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {}
+function Form() {
+  return (
+  <div className="add-form">
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+  <footer>
+    <em>💼 You have X items on your list, and you already packed X (X%)</em>
+  </footer>
+  );
+}
