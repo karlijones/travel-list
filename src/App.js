66ad058f7156493f4ee39ec694fbf405c7d6248f
karlijones <karlijones1984@gmail.com>
2024-10-16 21:09:59 -0500

    functions added to export default function App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-export default function App() {}
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+}
 
 function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
