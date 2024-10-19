801c54ca3909c1b8f4e9cad1496cc979e06a758d
karlijones <karlijones1984@gmail.com>
2024-10-19 12:30:55 -0500

    event.target.value

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -17,6 +19,7 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -33,7 +36,8 @@ function Form() {
         </option>
       ))}
     </select>
-    <input type="text" placeholder="Item..." />
+    <input type="text" placeholder="Item..." value={description} 
+    onChange={(e)=>setDescription(e.target.value)} />
     <button>Add</button>
   </form>
   );
