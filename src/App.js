c6c6b84927e4433cfc70eef202a53fada32da005
karlijones <karlijones1984@gmail.com>
2024-10-19 11:03:48 -0500

    select element with options

--- src/App.js
+++ src/App.js
@@ -20,6 +20,11 @@ function Form() {
   return (
   <form className="add-form">
     <h3>What do you need for your 😍 trip?</h3>
+    <select>
+      <option value={1}>1</option>
+      <option value={2}>2</option>
+      <option value={3}>3</option>
+    </select>
   </form>
   );
 }
