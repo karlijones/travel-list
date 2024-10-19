ec8c151ed8ee271410cf74a22175d8de0a84be13
karlijones <karlijones1984@gmail.com>
2024-10-19 12:49:33 -0500

    guard clause to catch no description

--- src/App.js
+++ src/App.js
@@ -20,15 +20,19 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-  }
+
+    if (!description) return;
 
     const newItem = { description, quantity, packed: 
       false, id: Date.now()};
       console.log(newItem);
+  }
+
+    
 
   return (
   <form className="add-form" onSubmit={handleSubmit}>
