4ebcf4d693ead3c9857da5b725bf4474f62aa6ba
karlijones <karlijones1984@gmail.com>
2024-10-20 13:41:04 -0500

    additional item added to initialItems

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false},
 ];
 
 export default function App() {
