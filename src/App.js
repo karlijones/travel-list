ff89b8bc72a838e1b2d70cf396ccc8b628a6d86d
karlijones <karlijones1984@gmail.com>
2024-10-21 20:41:05 -0500

    function for deleting items

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems(items=>items.filter(item=>item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
