1a2bf183416b6a13cd53a020962a5d290dfa633f
karlijones <karlijones1984@gmail.com>
2024-10-25 18:52:01 -0500

    sorting options added

--- src/App.js
+++ src/App.js
@@ -89,6 +89,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
         key={item.id} />
         ))}
       </ul>
+
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
