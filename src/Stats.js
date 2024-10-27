b14a68cbbae24567a1e7089c50821c31fe218917
karlijones <karlijones1984@gmail.com>
2024-10-27 11:12:38 -0500

    debugging

--- src/Stats.js
+++ src/Stats.js
@@ -14,10 +14,10 @@ export default function Stats({ items }) {
   return (
     <footer className="stats">
       <em>
-        {percentage === 100 ? "You got everything! Ready to go ✈️" :
-          `💼 You have {numItems} items on your list, and you already packed
-      ${numPacked} ${percentage}%`)}
+      {percentage === 100 ? 
+      "You got everything! Ready to go ✈️" : 
+      `💼 You have ${numItems} items on your list, and you already packed
+       ${numPacked} ${percentage}%`}
       </em>
-    </footer>
-  );
+    </footer>);
 }
