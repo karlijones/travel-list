4b2d18635fd7b0f889527225680616846bad6b0b
karlijones <karlijones1984@gmail.com>
2024-10-27 11:41:25 -0500

    debugging

--- src/Stats.js
+++ src/Stats.js
@@ -1,4 +1,8 @@
 export default function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   if (!items.length)
     return (
       <p className="stats">
@@ -7,17 +11,15 @@ export default function Stats({ items }) {
       </p>
     );
 
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked / numItems) * 100);
-
   return (
     <footer className="stats">
       <em>
-      {percentage === 100 ? 
-      "You got everything! Ready to go ✈️" : 
-      `💼 You have ${numItems} items on your list, and you already packed
-       ${numPacked} ${percentage}%`}
+      {percentage === 100 
+      ? "You got everything! Ready to go ✈️" 
+      : ` 💼 You have ${numItems} items on your list, 
+      and you already packed ${numPacked} (${percentage}%)`
+      }
       </em>
-    </footer>);
+    </footer>
+  );
 }
