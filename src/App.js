2460aba0cd4271f309d6c777dc8a348ef66d822f
karlijones <karlijones1984@gmail.com>
2024-10-26 20:10:02 -0500

    sort by packed variable

--- src/App.js
+++ src/App.js
@@ -87,9 +87,11 @@ function PackingList({ items, onDeleteItem,
     if (sortBy === "input") sortedItems = items;
 
     if (sortBy === "description") 
-      sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+      sortedItems = items.slice().sort((a,b) => 
+      a.description.localeCompare(b.description));
 
-    
+    if (sortBy === "packed") sortedItems = 
+    items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
 
     return (
       <div className="list">
